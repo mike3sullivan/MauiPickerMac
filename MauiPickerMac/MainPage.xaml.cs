21b43fda72239d25abeacaa5ef4f5baaf86cbbf0
mike3sullivan <mike.3.sullivan@gmail.com>
2022-09-19 15:37:36 -0400

    Add files via upload

---
+++ MauiPickerMac/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiPicker;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = this;
+	}
+
+	string selection = null;
+	public string Selection
+	{
+		get
+		{ 
+			return selection;
+		} 
+		set 
+		{ 
+			selection = value;
+			OnPropertyChanged(nameof(Selection));
+		}
+	}
+}
+
